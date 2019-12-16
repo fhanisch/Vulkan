f3aa8e6abdb14cb5ee69b06551b81c8f6a747dde
fhanisch <felix.hanisch@googlemail.com>
2019-12-16 21:36:23 +0100

    create dir

--- VulkanLib/app.mk
+++ VulkanLib/app.mk
@@ -2,17 +2,17 @@ VK_INC=/home/felix/Entwicklung/vulkan-sdk/1.1.126.0/source/Vulkan-Headers/includ
 dst=build/linux
 obj=$(dst)/main.cpp.o $(dst)/Window.cpp.o $(dst)/VulkanSetup.cpp.o $(dst)/Models.cpp.o $(dst)/matrix.c.o
 
-#main.h:
-#	touch $@
-
 $(dst):
 	mkdir -p $(dst)
+	touch src/main.h
 
-$(dst)/%.c.o: src/%.c src/%.h $(dst)
+$(dst)/%.c.o: src/%.c src/%.h
 	clang -o $@ -c $< -Wall -I $(VK_INC) -DLINUX
 
-$(dst)/%.cpp.o: src/%.cpp src/%.h $(dst)
+$(dst)/%.cpp.o: src/%.cpp src/%.h
 	clang++ -o $@ -c $< -Wall -I $(VK_INC) -DLINUX
 
 app: $(obj)
 	clang++ -o app $^ -Wall -ldl -lX11 -lpthread
+
+dir: $(dst)
